8e3f1d24b02e5241a59b2b4b040389bb89a60cbf
Anton Virtanen <anton.virtanen@tuni.fi>
2025-03-11 16:46:14 +0200

    Testing TB with bad DUT

--- top.sv
+++ top.sv
@@ -1,9 +1,9 @@
-module test(
+module top(
     input logic in_signal,
     output logic out_signal1,
     output logic out_signal2
 );
 	// The input is driven to outputs 1 and 2
-	assign out_signal1 = in_signal;
-	assign out_signal2 = in_signal;
+	assign out_signal1 != in_signal;
+	assign out_signal2 != in_signal;
 endmodule
