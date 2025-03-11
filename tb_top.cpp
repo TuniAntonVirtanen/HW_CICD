e0007a2cc1ae7dd386f4d19aadf10dcbfbf46594
Anton Virtanen <anton.virtanen@tuni.fi>
2025-03-11 17:25:48 +0200

    Change to TB to test only output1

--- tb_top.cpp
+++ tb_top.cpp
@@ -17,13 +17,12 @@ int main() {
 	
     std::cout << "Test 1: Input=0, Output1=" << out1 << ", Output2=" << out2 << std::endl;
 
-	if (out1 != 0 || out2 != 0) {
+	if (out1 != 0) { //  || out2 != 0
         std::cerr << "Test 1 failed!" << std::endl;
         return 1; // Exit with error
     }
 
     // Test 2: inputting 1
-	/*
     top->in_signal = 1;
     top->eval();
 	
@@ -32,11 +31,11 @@ int main() {
 	
     std::cout << "Test 2: Input=1, Output1=" << out1 << ", Output2=" << out2 << std::endl;
 
-    if (out1 != 1 || out2 != 1) {
+    if (out1 != 1) { //  || out2 != 1
         std::cerr << "Test 2 failed!" << std::endl;
         return 1; // Exit with error
     }
-	*/
+
     std::cout << "All tests passed!" << std::endl;
 
     delete top;
