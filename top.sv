40c1319d680d59afe10561c5b63c797cc876f797
unknown <anton.virtanen@tuni.fi>
2025-04-07 15:06:05 +0300

    Testing with bad DUT

--- top.sv
+++ top.sv
@@ -4,6 +4,6 @@ module top(
     output logic out_signal2
 );
 	// The input is driven to outputs 1 and 2
-	assign out_signal1 = (in_signal == 1) ? 0 : 1;
+	assign out_signal1 = in_signal;
 	assign out_signal2 = (in_signal == 1) ? 0 : 1;
 endmodule
